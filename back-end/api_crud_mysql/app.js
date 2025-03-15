874afe5c7a9f734047d7c61aff463327d69dae47
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-03-15 13:48:33 -0300

    feat: API with CRUD in MySQL

---
+++ back-end/api_crud_mysql/app.js
@@ -0,0 +1,57 @@
+const { createUser, readUsers, updateUser, deleteUser } = require("../../database/CRUD_NodeJS/main.js");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    const response = readUsers((err, response) => {
+        if (err) {
+            res.status(500).json({ "error": "Error fetching users", "details": err });
+        } else {
+            res.status(200).json(response);
+        }
+    });
+});
+
+app.post("/", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const response = createUser(name, email, password, (err, response) => {
+        if (err) {
+            res.status(500).json({ "error": "Error creating user", "details": err });
+        } else {
+            res.status(200).json(response);
+        }
+    });
+});
+
+app.put("/", (req, res) => {
+    const { id, name, email, password } = req.body;
+
+    const response = updateUser(id, name, email, password, (err, response) => {
+        if (err) {
+            res.status(500).json({ "error": "Error updating user", "details": err });
+        } else {
+            res.status(200).json(response);
+        };
+    });
+});
+
+app.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const response = deleteUser(id, (err, response) => {
+        if (err) {
+            res.status(500).json({ "error": "Error deleting user", "details": err });
+        } else {
+            res.status(200).json(response);
+        }
+    });
+});
+
+app.listen(port)
